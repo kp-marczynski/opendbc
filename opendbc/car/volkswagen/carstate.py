9bc64fc6dcdaa375d8c8a4be2966f152d9de9805
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-09-07 11:13:00 +0200

    Revert "Update carstate.py use Motor_51 for Accelerator detection"
    This reverts commit fe4b784408ee39a54b8fe3964e5080cba3243d3e.

--- opendbc/car/volkswagen/carstate.py
+++ opendbc/car/volkswagen/carstate.py
@@ -290,7 +290,7 @@ class CarState(CarStateBase, MadsCarState):
     #ret.carFaultedNonCritical = cam_cp.vl["EA_01"]["EA_Funktionsstatus"] in (3, 4, 5, 6) # prepared, not tested
 
     # Update gas, brakes, and gearshift.
-    ret.gasPressed   = pt_cp.vl["Motor_51"]["Accelerator_Pressure"] > 0
+    ret.gasPressed   = pt_cp.vl["Motor_54"]["Accelerator_Pressure"] > 0
     ret.brakePressed = bool(pt_cp.vl["Motor_14"]["MO_Fahrer_bremst"]) # includes regen braking by user
     ret.brake        = pt_cp.vl["ESC_51"]["Brake_Pressure"]
 
