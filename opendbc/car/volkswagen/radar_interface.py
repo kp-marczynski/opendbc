60c858fc696abd3c775a39b2c5a32143af4b5501
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-09-08 18:09:38 +0200

    Update radar_interface.py

--- opendbc/car/volkswagen/radar_interface.py
+++ opendbc/car/volkswagen/radar_interface.py
@@ -23,7 +23,7 @@ SIGNAL_SETS = tuple(
 
 
 def get_radar_can_parser(CP):
-  if CP.flags & (VolkswagenFlags.MEB || VolkswagenFlags.MQB_EVO):
+  if CP.flags & (VolkswagenFlags.MEB | VolkswagenFlags.MQB_EVO):
     messages = [("MEB_Distance_01", 25)]
   else:
     return None
