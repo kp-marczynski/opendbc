f0a275c083782452720ed084380656aa12a8a2dc
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-09-08 18:16:24 +0200

    Update carstate.py fix

--- opendbc/car/volkswagen/carstate.py
+++ opendbc/car/volkswagen/carstate.py
@@ -301,11 +301,11 @@ class CarState(CarStateBase, MadsCarState):
 
     # Update door and trunk/hatch lid open status.
     doors = pt_cp.vl["ZV_02"] if bool(pt_cp.vl["Gateway_72"]["ZV_02_alt"]) else pt_cp.vl["Gateway_72"]
-    ret.doorOpen = any(doors["ZV_FT_offen"],
-                       doors["ZV_BT_offen"],
-                       doors["ZV_HFS_offen"],
-                       doors["ZV_HBFS_offen"],
-                       doors["ZV_HD_offen"])
+    ret.doorOpen = any([doors["ZV_FT_offen"],
+                        doors["ZV_BT_offen"],
+                        doors["ZV_HFS_offen"],
+                        doors["ZV_HBFS_offen"],
+                        doors["ZV_HD_offen"]])
 
     # Update seatbelt fastened status.
     ret.seatbeltUnlatched = pt_cp.vl["Airbag_02"]["AB_Gurtschloss_FA"] != 3
